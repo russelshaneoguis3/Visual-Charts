a83ef4574f63e9544de9bdb34b66a6de350d97ca
russelshaneoguis3 <126945000+russelshaneoguis3@users.noreply.github.com>
2023-12-12 22:57:05 +0800

    Add files via upload

---
+++ doh-epi-dengue-data-2016-2021.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 11, 2023 at 12:48 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dengue`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doh-epi-dengue-data-2016-2021`
+--
+-- Error reading structure for table dengue.doh-epi-dengue-data-2016-2021: #1146 - Table &#039;dengue.doh-epi-dengue-data-2016-2021&#039; doesn&#039;t exist
+-- Error reading data for table dengue.doh-epi-dengue-data-2016-2021: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dengue`.`doh-epi-dengue-data-2016-2021`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
